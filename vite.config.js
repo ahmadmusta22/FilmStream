283b8dd625c50ec7cd4528cf37799a5ef241ce9c
ahmadmusta22 <ahmadmastersito2@gmail.com>
2025-02-16 05:06:34 +0200

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/movieapp/",
+  base: "/movieapp/", // Change to your repository name
   plugins: [react()],
 });
