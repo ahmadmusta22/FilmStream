3b1f010adaaf5a6fcf47d1b4a0c8493efe0b169f
ahmadmusta22 <ahmadmastersito2@gmail.com>
2025-02-16 04:49:02 +0200

    fixing bugs

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,27 +1,12 @@
-// import { applyMiddleware, createStore } from 'redux';
-// import { combineReducers } from 'redux';
-// import favoritesReducer from './favoritesReducer';
-// import { thunk } from 'redux-thunk';
-
-
-// const rootReducer = combineReducers({
-//   favorites: favoritesReducer, // favorites reducer
-// });
-
-// const store = createStore(rootReducer, applyMiddleware(thunk));
-
-// export default store;
-
-
 import { configureStore } from '@reduxjs/toolkit';
 import favoritesReducer from './favoritesSlice';
+ // Ensure this path is correct
 
 const store = configureStore({
   reducer: {
-    favorites: favoritesReducer,
-    
-    devTools: process.env.NODE_ENV !== 'production', 
+    favorites: favoritesReducer, // ✅ Correct
   },
+  devTools: process.env.NODE_ENV !== 'production', // ✅ Moved outside reducer
 });
 
 export default store;
